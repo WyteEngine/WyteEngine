879cd0390e55312f25c84f3e1fd56a26984b43b4
Xeltica <citringo@gmail.com>
2018-02-15 20:40:39 +0900

    フラグ管理の内部仕様を変更(API変更なし)

--- Assets/Wyte/Event/FlagManager.cs
+++ Assets/Wyte/Event/FlagManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Novel.Exceptions;
 using System.Linq;
-using FlagMap = System.Collections.Generic.Dictionary<string, bool>;
+using FlagMap = FlagDictionary;
 
 public class FlagManager : SingletonBaseBehaviour<FlagManager>
 {
@@ -20,6 +20,9 @@ public class FlagManager : SingletonBaseBehaviour<FlagManager>
 	/// </summary>
 	FlagMap areaFlags;
 
+	public FlagMap Flags => flags;
+	public FlagMap SkipFlags => skipFlags;
+	public FlagMap AreaFlags => areaFlags;
 
 	protected override void Awake()
 	{
@@ -42,23 +45,23 @@ public class FlagManager : SingletonBaseBehaviour<FlagManager>
 
 	}
 
-	private void FlagImpl(Dictionary<string, bool> flagDic, string[] args)
+	private void FlagImpl(FlagMap flagDic, string[] args)
 	{
 		if (args.Length < 2)
 			throw new NRuntimeException("引数が足りません。");
 		flagDic[args[1]] = args[0] == "on";
 	}
 
-	private IEnumerator OnFlagImpl(Dictionary<string, bool> flagDic, string key, string[] labels)
+	private IEnumerator OnFlagImpl(FlagMap flagDic, string key, string[] labels)
 	{
 		if (labels.Length < 1)
 			return null;
-		if (flagDic.ContainsKey(key) && flagDic[key])
+		if (flagDic[key])
 			return Novel.Runtime.Goto("", labels[0]);
 		if (labels.Length >= 2)
 			return Novel.Runtime.Goto("", labels[1]);
 		return null;
-	
+
 	}
 
 	public IEnumerator Flag(string _, string[] args)
@@ -94,4 +97,50 @@ public class FlagManager : SingletonBaseBehaviour<FlagManager>
 	{
 		yield return OnFlagImpl(areaFlags, name, labels);
 	}
+}
+
+public sealed class FlagDictionary : IDictionary<string, bool>
+{
+	private IDictionary<string, bool> dic;
+
+	public FlagDictionary()
+	{
+		dic = new Dictionary<string, bool>();
+	}
+
+	public bool this[string key] 
+	{ 
+		get { return dic.ContainsKey(key) && dic[key]; }
+		set { dic[key] = value; } 
+	}
+
+	public ICollection<string> Keys => dic.Keys;
+
+	public ICollection<bool> Values => dic.Values;
+
+	public int Count => dic.Count;
+
+	public bool IsReadOnly => false;
+
+	public void Add(string key, bool value) => dic[key] = value;
+
+	public void Add(KeyValuePair<string, bool> item) => dic[item.Key] = item.Value;
+
+	public void Clear() => dic.Clear();
+
+	public bool Contains(KeyValuePair<string, bool> item) => dic.Contains(item);
+
+	public bool ContainsKey(string key) => dic.ContainsKey(key);
+
+	public void CopyTo(KeyValuePair<string, bool>[] array, int arrayIndex) => dic.CopyTo(array, arrayIndex);
+
+	public IEnumerator<KeyValuePair<string, bool>> GetEnumerator() => dic.GetEnumerator();
+
+	public bool Remove(string key) => dic.Remove(key);
+
+	public bool Remove(KeyValuePair<string, bool> item) => dic.Remove(item);
+
+	public bool TryGetValue(string key, out bool value) => dic.TryGetValue(key, out value);
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
